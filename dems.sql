0a8688564dc254750012ce71d0f12ad269ea2fee
callumbeer <c.beer2@uni.brighton.ac.uk>
2022-05-01 19:38:07 +0100

    Update dems.sql

--- dems.sql
+++ dems.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2022 at 11:01 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Generation Time: May 01, 2022 at 08:37 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `vms`
+-- Database: `dems`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `case`
+--
+
+CREATE TABLE `case` (
+  `case_no` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `case_type` varchar(255) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `createdBy` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `case`
+--
+
+INSERT INTO `case` (`case_no`, `title`, `description`, `case_type`, `date_created`, `createdBy`) VALUES
+('1', 'R v Smith', '<p>Smith attacked a police officer killing him in the process<br></p>', 'Murder', '2022-04-30 23:00:00', '1234');
 
 -- --------------------------------------------------------
 
@@ -31,27 +53,16 @@ CREATE TABLE `file` (
   `id` int(11) NOT NULL,
   `file_name` varchar(255) NOT NULL,
   `file_type` varchar(255) NOT NULL,
-  `vehicle_reg` varchar(255) NOT NULL
+  `case_no` varchar(255) NOT NULL,
+  `file_hash` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `file`
 --
 
-INSERT INTO `file` (`id`, `file_name`, `file_type`, `vehicle_reg`) VALUES
-(1, 'default_icon.png', 'image', '0'),
-(2, '-0-0.jpg', 'image', '0'),
-(3, '-0-1.jpg', 'image', '0'),
-(4, '-0-0.jpg', 'image', '0'),
-(5, '-0-1.jpg', 'image', '0'),
-(6, 'SSPDD-0-0.jpg', 'image', '0'),
-(13, 'XXX-XXX-0.jpg', 'image', 'XXX'),
-(14, 'XXX-XXX-1.jpg', 'image', 'XXX'),
-(15, '', 'video', 'ZZZ'),
-(17, 'ZZZ-ZZZ-0.jpg', 'image', 'ZZZ'),
-(18, 'Sportage-Sportage.butterfly.mp4', 'video', 'Sportage'),
-(19, 'Sportage-Sportage-0.jpg', 'image', 'Sportage'),
-(20, 'Sportage-Sportage-1.jpg', 'image', 'Sportage');
+INSERT INTO `file` (`id`, `file_name`, `file_type`, `case_no`, `file_hash`) VALUES
+(38, 'default_icon.png', 'image', '1', '');
 
 -- --------------------------------------------------------
 
@@ -60,14 +71,14 @@ INSERT INTO `file` (`id`, `file_name`, `file_type`, `vehicle_reg`) VALUES
 --
 
 CREATE TABLE `user` (
-  `pass_number` varchar(255) NOT NULL,
+  `badge_number` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` text NOT NULL,
   `phone_number` int(11) NOT NULL,
   `account_type` varchar(255) NOT NULL DEFAULT 'normal',
-  `created_on` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_on` datetime NOT NULL,
   `last_login` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -75,37 +86,18 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`pass_number`, `first_name`, `surname`, `email`, `password`, `phone_number`, `account_type`, `created_on`, `last_login`) VALUES
-('1', 'admin', 'aaa', 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 123456, 'admin', '2022-03-05 23:17:48', '0000-00-00 00:00:00'),
-('DF122', 'DEF', 'Ru', 'abc@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 123456, 'd41d8cd98f00b204e9800998ecf8427e', '2022-03-07 23:30:30', '0000-00-00 00:00:00'),
-('J45', 'John', 'Smith', 'john@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 12345, 'normal', '2022-03-07 23:07:24', '0000-00-00 00:00:00'),
-('KD13', 'KD', 'KD', 'KD@gmail.com', '25d55ad283aa400af464c76d713c07ad', 12345678, 'normal', '2022-03-08 17:52:53', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vehicle`
---
-
-CREATE TABLE `vehicle` (
-  `vehicle_reg` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `fuel_type` varchar(255) NOT NULL,
-  `year` int(11) NOT NULL,
-  `createdBy` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `vehicle`
---
-
-INSERT INTO `vehicle` (`vehicle_reg`, `title`, `description`, `fuel_type`, `year`, `createdBy`) VALUES
-('Sportage', 'Sportage', '<p>Sportage desc</p>', 'diesel', 2021, '1');
+INSERT INTO `user` (`badge_number`, `first_name`, `surname`, `email`, `password`, `phone_number`, `account_type`, `created_on`, `last_login`) VALUES
+('123', 'Admin', 'A', 'admin@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 123456789, 'admin', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `case`
+--
+ALTER TABLE `case`
+  ADD PRIMARY KEY (`case_no`);
 
 --
 -- Indexes for table `file`
@@ -117,13 +109,7 @@ ALTER TABLE `file`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`pass_number`);
-
---
--- Indexes for table `vehicle`
---
-ALTER TABLE `vehicle`
-  ADD PRIMARY KEY (`vehicle_reg`);
+  ADD PRIMARY KEY (`badge_number`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -133,7 +119,7 @@ ALTER TABLE `vehicle`
 -- AUTO_INCREMENT for table `file`
 --
 ALTER TABLE `file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
